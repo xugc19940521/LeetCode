2e5bc1fe10b800523c850e5b8118d212e038dd6e
xugc19940521 <984588081@qq.com>
2019-06-06 15:02:15 +0800

    题目557实现

---
+++ src/leetcode/question10/FindContentChildren.java
@@ -0,0 +1,42 @@
+package leetcode.question10;
+
+import java.util.Arrays;
+
+public class FindContentChildren {
+
+    public static int findContentChildren(int[] g, int[] s) {
+        return method2(g,s);
+    }
+
+    public static int method1(int[] g, int[] s){
+        int num = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        for(int i=0;i<g.length;i++){
+            for(int j=0;j<s.length;j++){
+                if(g[i] <= s[j]){
+                    s[j] = 0;
+                    num++;
+                    break;
+                }
+            }
+        }
+        return num;
+    }
+
+    public static int method2(int[] g, int[] s){
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int child=0;
+        int cookie=0;
+        while(child<g.length && cookie<s.length)
+        {
+            if(g[child]<=s[cookie])
+            {
+                child++;
+            }
+            cookie++;
+        }
+        return child;
+    }
+}
